38ac2d4b7e06b875ced49d2bf936ad8fbc92e10b
user <user@Sloboda.(none)>
2014-07-17 15:48:48 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Burzak::Application.config.secret_key_base = '770976a9bc4347089fc0585eab03dba4ecc282eb8d7cb2040ae968f2ae3d18c2eaad415a4e23eaaa66b8fcbe25f236391e0ed7c9bdeec71ac80d399aa6b33e0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Burzak::Application.config.session_store :cookie_store, key: '_burzak_session'
